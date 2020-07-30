75207050c7833d0957c8c698e29c47d64f49a25d
AD <askatder1991@gmail.com>
2020-07-29 21:05:13 -0500

    first commit

---
+++ src/Hello_Batch20.java
@@ -0,0 +1,10 @@
+public class Hello_Batch20 {
+
+    public static void main(String[] args) {
+
+        System.out.println("HELLO my batch-20 did you learn terminal how to use git? -from your phone:");
+
+
+        //added lines
+    }
+}
